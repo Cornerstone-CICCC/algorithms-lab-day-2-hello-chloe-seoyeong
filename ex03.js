1cee839f263428da610e178955f5cf97fcad9e9f
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 09:09:20 -0700

    ex03 completed

--- ex03.js
+++ ex03.js
@@ -2,4 +2,17 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = function (arr) {
+  const nums = arr;
+
+  const missingNum = nums.reduce((num, current, index) => {
+    if(current !== (index + 1)) {
+      return num = index;
+    }
+    return num = num;
+  }, 1)
+
+  return missingNum;
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
