c44c83e5af0292c9ee4414c54646b807ef497755
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 14:06:13 -0700

    ex09 completed

--- ex09.js
+++ ex09.js
@@ -3,4 +3,15 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
+const sortByProperty = function (arr, prop) {
+  let sorting = arr; 
+  let property = prop;
+
+  sorting.sort(function(obj1, obj2) {
+    return obj1[property] - obj2[property];
+  })
+
+  return sorting;
+}
+
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
