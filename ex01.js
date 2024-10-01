df36aa9497aa51e67f2c6fa87a083fd1837e1dc7
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 08:54:44 -0700

    ex01 completed

--- ex01.js
+++ ex01.js
@@ -2,4 +2,19 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
+const capitalizeWords = function (sentence) {
+  const words = sentence.split(" ");
+
+  const capitalize = words.map((word) => {
+    let character = word.split("");
+    character[0] = character[0].toUpperCase();
+    character = character.join("");
+
+    return character;
+  })
+
+  return capitalize.join(" ");
+}
+
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
+console.log(capitalizeWords("algorithms lab day")) //Expected output: "Algorithms Lab Day"
