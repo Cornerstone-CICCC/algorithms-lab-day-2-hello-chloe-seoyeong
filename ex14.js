ee8d78b55905b375926d24ce53f510fe947d0443
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 09:36:11 -0700

    ex14 completed

--- ex14.js
+++ ex14.js
@@ -2,6 +2,19 @@
 // `addItem(item)` to add an item to the cart, and `getItems()` to retrieve the list of added items.
 // Example: const cart = createCart(); cart.addItem("apple"); cart.getItems(); // ["apple"]
 
+function createCart () {
+
+  return {
+    items: [],
+    addItem: function(item) {
+      return this.items.push(item);
+    },
+    getItems: function() {
+      return this.items;
+    }
+  }
+}
+
 const cart = createCart();
 cart.addItem("apple");
 cart.addItem("banana");
