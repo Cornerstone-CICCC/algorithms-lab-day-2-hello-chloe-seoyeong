707be8be27d2e49649f49e794c58632c3869b05f
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 09:47:37 -0700

    ex11 completed

--- ex11.js
+++ ex11.js
@@ -2,4 +2,17 @@
 // and returns the number of times the word appears in the string.
 // Example: countWordOccurrences("hello world hello", "hello") should return 2.
 
+const countWordOccurrences = function (string, word) {
+  const splitString = string.split(" ");
+  let count = 0;
+
+  splitString.forEach(element => {
+    if(element === word) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
 console.log(countWordOccurrences("hello world hello", "hello")); // Expected output: 2
