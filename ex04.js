1d64c3067c5017e65cc5f71e6f0f98b7be8947b8
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 13:42:17 -0700

    ex04 completed

--- ex04.js
+++ ex04.js
@@ -2,4 +2,22 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+const mostFrequentChar = function (word) {
+  const characters = word.split("");
+  
+  const characterObj = characters.reduce((obj, current) => {
+    if(!obj[current]) {
+      obj[current] = 1;
+    } else {
+      obj[current] += 1;
+    }
+    
+    return obj;
+  }, {})
+
+  const character = Object.keys(characterObj).reduce((char, current) => characterObj[current] > characterObj[char] ? current : char);
+
+  return character;
+}
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
