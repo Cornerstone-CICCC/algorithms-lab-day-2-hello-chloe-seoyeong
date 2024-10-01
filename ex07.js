2f8fd86fd597837fa3e00c270d93234b7a1d5c3c
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 10:38:17 -0700

    ex07 completed

--- ex07.js
+++ ex07.js
@@ -3,5 +3,27 @@
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
 
+const validateEmail = function (email) {
+  let correctAt = false;
+  let correctDot = false;
+
+  const splitEmail = email.split("");
+
+  splitEmail.map(element => {
+    if (element === "@") {
+      correctAt = true;
+    }
+    if (element === ".") {
+      correctDot = true;
+    }
+  });
+
+  if (correctAt && correctDot) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
