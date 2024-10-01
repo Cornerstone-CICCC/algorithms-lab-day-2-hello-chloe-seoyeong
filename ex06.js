584edf3492ffc4c71340b1030ece207ecf937889
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-10-01 10:47:38 -0700

    ex06 completed

--- ex06.js
+++ ex06.js
@@ -3,4 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+const getQueryParams = function (url) {
+  const urls = url.split("?");
+  const parameters = urls[1];
+  const parameterArray = parameters.split("&");
+  const parameter = parameterArray.map(element => {
+    return element.split("=");
+  })
+
+  const params = parameter.reduce((obj, current) => {
+    obj[current[0]] = current[1];
+
+    return obj;
+  }, {})
+
+  return params;
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
